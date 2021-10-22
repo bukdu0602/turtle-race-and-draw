60c055e1dda2e332720399d4541dfd9891216da1
Ryan Lim <ryan.hyun@gmail.com>
2021-10-22 15:26:28 -0700

    oct 22

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ while is_race_on:
                 print("You lose")
 
 
-# asdfasdfsdfasdfuyihjkjkl345asdfsdfafdsfeasdsadfsdfasdf
+# asdfasdfsdfasdfuyihjkjkl345asdfsdfafdsfeasdsadfsdfasdfasdf
 
 
 screen.exitonclick()
